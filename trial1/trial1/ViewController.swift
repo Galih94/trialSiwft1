0beaad19d23b1161eb2e548fe148d84d9b80e77b
Galih Samodra Wicaksono <galihsamodra@Galihs-MacBook-Air.local>
2018-05-10 14:49:13 +0700

    Initial push

---
+++ trial1/trial1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  trial1
+//
+//  Created by Galih Samodra Wicaksono on 5/10/18.
+//  Copyright © 2018 Galih Samodra Wicaksono. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
